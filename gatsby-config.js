cb5b9fb45c6e629f360356579661ca759c1e224c
Chadd Poggenpoel <chaddpoggenpoel@gmail.com>
2020-06-24 10:30:53 +0200

    0.0.1

---
+++ gatsby-config.js
@@ -0,0 +1,32 @@
+const basePath = `/`
+const mdx = true
+
+module.exports = {
+  siteMetadata: {
+    siteTitle: `Chadd Web Design`,
+    siteUrl: `https://chaddwebdesign.co.za`,
+    siteDescription: `I help entrepreneurs gain a competitive edge in the online world with my web design and pay-per-click campaign management services.`,
+    siteLanguage: `en-ZA`,
+    author: `Chadd Poggenpoel <chaddwebdesign@gmail.com>`,
+    basePath,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `sections`,
+        path: `${__dirname}/src/sections`,
+      },
+    },
+    mdx && {
+      resolve: `gatsby-plugin-mdx`,
+      options: {},
+    },
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-typescript`,
+    `gatsby-plugin-emotion`,
+    `gatsby-plugin-theme-ui`,
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-netlify`,
+  ],
+}
---
+++ src/hooks/use-site-metadata.tsx
@@ -0,0 +1,34 @@
+import { graphql, useStaticQuery } from "gatsby"
+
+type UseSiteMetadataProps = {
+  site: {
+    siteMetadata: {
+      siteTitle: string
+      siteUrl: string
+      siteDescription: string
+      siteLanguage: string
+      author: string
+      [key: string]: unknown
+    }
+  }
+}
+
+const useSiteMetadata = () => {
+  const data = useStaticQuery<UseSiteMetadataProps>(graphql`
+    query {
+      site {
+        siteMetadata {
+          siteTitle
+          siteUrl
+          siteDescription
+          siteLanguage
+          author
+        }
+      }
+    }
+  `)
+
+  return data.site.siteMetadata
+}
+
+export default useSiteMetadata

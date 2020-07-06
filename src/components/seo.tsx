0852003f84392c79b842766ef5b53686bb7661a7
Chadd Poggenpoel <chaddpoggenpoel@gmail.com>
2020-07-06 11:51:25 +0200

    0.0.1

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -7,7 +7,6 @@ const defaultProps = {
   title: ``,
   description: false,
   pathname: false,
-  image: false,
   children: null,
 }
 
@@ -19,7 +18,7 @@ type Props = {
   children?: React.ReactNode
 }
 
-const SEO = ({ title, description, pathname, image, children }: Props) => {
+const SEO = ({ title, description, pathname, children }: Props) => {
   const site = useSiteMetadata()
 
   const {
@@ -28,7 +27,6 @@ const SEO = ({ title, description, pathname, image, children }: Props) => {
     siteUrl,
     siteDescription: defaultDescription,
     siteLanguage,
-    siteImage: defaultImage,
     author,
   } = site
 
@@ -36,30 +34,47 @@ const SEO = ({ title, description, pathname, image, children }: Props) => {
     title: title || defaultTitle,
     description: description || defaultDescription,
     url: `${siteUrl}${pathname || ``}`,
-    image: `${siteUrl}${image || defaultImage}`,
   }
   return (
-    <Helmet title={title} defaultTitle={defaultTitle} titleTemplate={`%s | ${siteTitle}`}>
+    <Helmet
+      title={title}
+      defaultTitle={defaultTitle}
+      titleTemplate={`%s | ${siteTitle}`}
+    >
       <html lang={siteLanguage} />
       <meta name="description" content={seo.description} />
-      <meta name="image" content={seo.image} />
+      <meta name="image" content="/social.png" />
       <meta property="og:title" content={seo.title} />
       <meta property="og:url" content={seo.url} />
       <meta property="og:description" content={seo.description} />
-      <meta property="og:image" content={seo.image} />
+      <meta property="og:image" content="/social.png" />
       <meta property="og:type" content="website" />
       <meta property="og:image:alt" content={seo.description} />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={seo.title} />
       <meta name="twitter:url" content={seo.url} />
       <meta name="twitter:description" content={seo.description} />
-      <meta name="twitter:image" content={seo.image} />
+      <meta name="twitter:image" content="/social.png" />
       <meta name="twitter:image:alt" content={seo.description} />
       <meta name="twitter:creator" content={author} />
       <meta name="gatsby-theme" content="@lekoarts/gatsby-theme-cara" />
-      <link rel="icon" type="image/png" sizes="32x32" href={withPrefix(`/favicon-32x32.png`)} />
-      <link rel="icon" type="image/png" sizes="16x16" href={withPrefix(`/favicon-16x16.png`)} />
-      <link rel="apple-touch-icon" sizes="180x180" href={withPrefix(`/apple-touch-icon.png`)} />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href={withPrefix(`/favicon-32x32.png`)}
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href={withPrefix(`/favicon-16x16.png`)}
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href={withPrefix(`/apple-touch-icon.png`)}
+      />
       {children}
     </Helmet>
   )
